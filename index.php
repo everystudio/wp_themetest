20b34c88c4deef8828f13a8704074693c7867a48
watanabe@holo <watanabe.everystudio@gmail.com>
2020-10-12 15:56:58 +0900

    パスとか変更

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" type="text/css" />
 <!--[if lt IE 9]>
 <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <script src="https://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
 <![endif]-->
-<title>サンプルサイト</title>
+<title><?php bloginfo('name'); ?></title>
 </head>
 <body class="home">
     <header class="globalHeader">
         <div class="inner">
-            <h1><a href="/"><img src="images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
+            <h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
             <p class="description">サイトのキャッチフレーズ</p>
             <form action="" method="get" class="searchform">
                 <input type="text" value="" name="s" id="s" placeholder="サイト内検索">
